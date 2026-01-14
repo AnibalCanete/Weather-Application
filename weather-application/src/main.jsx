e3a435371dd0ac8c9456d15c05c67269a1001a09
Anibal Cañete <anibal_gustavo2010@hotmail.com>
2026-01-13 22:41:21 -0300

    Updated index.html and main.jsx Files

--- weather-application/src/main.jsx
+++ weather-application/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { CssBaseline} from "@mui/material";
+import { SnackbarProvider } from "notistack";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SnackbarProvider>
+      <CssBaseline />
+      <App />
+    </SnackbarProvider>
   </StrictMode>,
 )
