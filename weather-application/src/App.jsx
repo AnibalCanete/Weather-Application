2cc3228bc6828dc49c5f9c3cdbfa39e5b77da818
Anibal Cañete <anibal_gustavo2010@hotmail.com>
2026-01-13 23:09:05 -0300

    Updated App.jsx and Dependencies

--- weather-application/src/App.jsx
+++ weather-application/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState } from "react";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ error: false, message: "", });
+  const [weather, setWeather] = useState({ city: "", country: "", temp: "", condition: "", icon: "", conditionText: "", });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError({ error: false, message: "", });
+    try {
+      if (!city.trim()) throw { message: "The City Field is Required."};
+      const response = await fetch(`${API_WEATHER}${city}`);
+      const data = await response.json();
+      if (data.error) throw { message: data.error.message };
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
+    } catch (error) {
+      setError({ error: true, message: error.message, });
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container-main">
+      <div className="container">
+        <Container maxWidth="xs" sx={{ mt: 2 }}>
+          <Typography variant="h3" component="h1" align="center" gutterBottom>Weather App</Typography>
+          <Box sx={{ display: "grid", gap: 2 }} component="form" autoComplete="off" onSubmit={onSubmit}>
+            <div className="form-text">
+              <TextField id="city" label="city" variant="outlined" size="small" required fullWidth value={city} onChange={(e) => setCity(e.target.value)} error={error.error} helperText={error.message} />
+            </div>
+            <Button type="submit" variant="contained" loading={loading} loadingIndicator="Loading...">Search</Button>
+          </Box>
+          {weather.city && (
+            <Box sx={{ mt: 2, display: "grid", gap: 2, textAlign: "center" }}>
+              <Typography variant="h4" component="h2">{weather.city}, {weather.country}</Typography>
+              <Box component="img" alt={weather.conditionText} src={weather.icon} sx={{ margin: "0 auto" }}></Box>
+              <Typography variant="h5" component="h3">{weather.temp} °C</Typography>
+              <Typography variant="h6" component="h4">{weather.conditionText}</Typography>
+            </Box>
+          )}
+          <Typography textAlign="center" sx={{ mt: 2, fontSize: "10px" }}>
+            Powered by: {" "}
+            <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a>
+          </Typography>
+        </Container>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
